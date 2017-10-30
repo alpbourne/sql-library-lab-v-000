3155eccf79317ad6ac0ef83da2ac4bdd636bffcd
alpbourne <alpbourne@gmail.com>
2017-10-30 18:28:59 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,9 +24,9 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
     FROM authors
-    JOIN series
+    INNER JOIN series
     ON authors.id = series.author_id
-    JOIN series
+    INNER JOIN series
     ON subgenres.id = series.subgenres_id;"
 end
 
